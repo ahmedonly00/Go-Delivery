e3bbbf7ce7c0aafc3bc0cf5ca1c859c935672010
ahmedonly00 <ahmedndayizeye@gmail.com>
2025-10-06 00:23:09 +0200

    fixing errors on operating hours

--- src/main/java/com/goDelivery/goDelivery/dtos/restaurant/OperatingHoursDTO.java
+++ src/main/java/com/goDelivery/goDelivery/dtos/restaurant/OperatingHoursDTO.java
@@ -1,10 +1,13 @@
 package com.goDelivery.goDelivery.dtos.restaurant;
 
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
+import lombok.*;
 
-
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OperatingHoursDTO {
     
     @Pattern(regexp = "^([01]?[0-9]|2[0-3]):[0-5][0-9]$", message = "Invalid time format. Use HH:MM")
@@ -13,5 +16,6 @@ public class OperatingHoursDTO {
     @Pattern(regexp = "^([01]?[0-9]|2[0-3]):[0-5][0-9]$", message = "Invalid time format. Use HH:MM")
     private String close;
     
+    @Builder.Default
     private boolean isClosed = false;
 }
