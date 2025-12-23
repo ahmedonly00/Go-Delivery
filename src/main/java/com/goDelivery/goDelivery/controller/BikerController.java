0a6325f1d9a4e12f723c57a96a1117c672a95582
ahmedonly00 <ahmedndayizeye@gmail.com>
2025-12-23 14:48:43 +0200

    fixing file type

--- src/main/java/com/goDelivery/goDelivery/controller/BikerController.java
+++ src/main/java/com/goDelivery/goDelivery/controller/BikerController.java
@@ -28,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
